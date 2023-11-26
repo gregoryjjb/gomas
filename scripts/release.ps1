6e83b21502fe1ad607b060ca0ea3b0c8a2ca8fca
Greg Brisebois <gregoryjjb@gmail.com>
2023-11-26 10:55:36 -0800

    Add release check for uncommitted changes

--- scripts/release.ps1
+++ scripts/release.ps1
@@ -9,13 +9,21 @@ if ($VERSION -notmatch '^v\d+\.\d+\.\d+$') {
   exit 1
 }
 
+# Make sure everything is committed
+if (git status --porcelain) {
+  Write-Host "There are uncommitted changes! Run `git status` for details. Aborting"
+  exit 1
+}
+
 # Build
 scripts/build.ps1 $VERSION
 if ($LastExitCode -ne 0) { exit 1 }
 
 # Update README
+Write-Host "Replacing version in readme: $PREVVERSION → $VERSION"
 (Get-Content readme.md).Replace($PREVVERSION, $VERSION) | Set-Content readme.md
 git add readme.md
+if ($LastExitCode -ne 0) { exit 1 }
 git commit -m "Release $VERSION"
 if ($LastExitCode -ne 0) { exit 1 }
 
