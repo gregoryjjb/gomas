8d670f2402a31e970f64de1bf5d2302240d7a374
Greg Brisebois <gregoryjjb@gmail.com>
2025-11-30 20:50:37 -0800

    Installation script

--- install.sh
+++ install.sh
@@ -15,14 +15,47 @@ fi
 
 echo "Detected architecture: $ARCH"
 
-# Download the appropriate archive to /tmp
-wget -O "/tmp/gomas-$ARCH.tgz" "https://github.com/gregoryjjb/gomas/releases/download/latest/gomas-$ARCH.tgz"
+echo "Downloading latest version of gomas"
+wget -q --show-progress -O "/tmp/gomas-$ARCH.tgz" "https://github.com/gregoryjjb/gomas/releases/latest/download/gomas-$ARCH.tgz"
+echo
 
-# Remove old binary and extract new one
+echo "Removing old version"
 sudo rm -rf /usr/local/bin/gomas
+
+echo "Installing"
 sudo tar -C /usr/local/bin -xzf "/tmp/gomas-$ARCH.tgz"
+echo
 
 # Verify installation
 echo "Gomas has been installed:"
 gomas --version
 echo
+
+SERVICE="gomas.service"
+
+if systemctl list-unit-files | grep -q "^$SERVICE"; then
+    read -p "Restart gomas service? (y/N): " RESTART
+    if [[ "$RESTART" =~ ^[Yy]$ ]]; then
+        sudo systemctl restart "$SERVICE"
+        echo "Service restarted."
+    else
+        echo "Service not restarted."
+    fi
+
+else
+    echo "No systemd service for gomas exists."
+
+    read -p "Would you like to create one? (y/N): " CREATE
+    if [[ "$CREATE" =~ ^[Yy]$ ]]; then
+        # TODO: create service file
+                exit
+
+        sudo systemctl daemon-reload
+        sudo systemctl enable gomas.service
+        sudo systemctl start gomas.service
+
+        echo "Service created and started."
+    else
+        echo "Service not created."
+    fi
+fi
